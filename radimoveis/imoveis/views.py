b68886b5f69bce31ed2757ebcbe2769a2a408958
rafaeldiasmello <rafaeldias@cpl.com.br>
2016-10-11 16:34:44 -0300

    Injecao do modelo(mockado) no template

--- radimoveis/imoveis/views.py
+++ radimoveis/imoveis/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
-
+from .models import Imovel
 
 def home(request):
-    return render(request, 'index.html')
+    context = {
+        'imoveis': [
+            Imovel(breve_descricao='Apartamento muito legal!',
+                   endereco='Rua Pedro Krisnki n 277',
+                   preco='10',
+                   detalhes="""O que temos que ter sempre em mente é que a constante'
+                            divulgação das informações afeta positivamente a correta
+                            previsão das condições financeiras e administrativas exigidas."""),
+            Imovel(breve_descricao='Casa Bacana em madureira',
+                   endereco='Madureira 881',
+                   preco='50',
+                   detalhes="""O que temos que ter sempre em mente é que a constante'
+                           divulgação das informações afeta positivamente a correta
+                           previsão das condições financeiras e administrativas exigidas."""),
+            Imovel(breve_descricao='Outra casa legal!',
+                   endereco='campo grande',
+                   preco='30',
+                   detalhes="Esse apartamento é tão legal cara, maneiro mesmo..."),
+            Imovel(breve_descricao='Casa no centro da cidade',
+                   endereco='Centro RJ',
+                   preco='40',
+                   detalhes="Esse aqui então, irado demais."),
+        ]
+    }
+    return render(request, 'index.html', context)
